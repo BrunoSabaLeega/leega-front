0f7646f1da519359017cd53c4b684abe83e3974a
BrunoSabaLeega <bruno.saba@leega.com.br>
2023-03-09 17:58:38 -0300

    terceira página

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ basePath = __dirname + "/views";
 routes.get('/', (request, response) => response.sendFile(basePath + "/main.html"))
 routes.get('/main.html', (request, response) => response.sendFile(basePath + "/main.html"))
 routes.get('/login.html', (request, response) => response.sendFile(basePath + "/login.html"))
+routes.get('/admin.html', (request, response) => response.sendFile(basePath + "/admin.html"))
 
 
 module.exports = routes;
