d48a65b4420ddc429ae5bd7cc5e380471955569e
BrunoSabaLeega <bruno.saba@leega.com.br>
2023-03-09 15:56:00 -0300

    first commit front leega

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ server.use(express.static("public"));
 server.use(routes);
 
 server.listen(3000, () => {
-    console.log('Server is booming on port, 3000 Visit http://localhost:3000');
+    console.log('Server is booming on port, 3000! Visit http://localhost:3000');
 })
