b406b7e362151e44818f8ebeff0eb7dd5b5b68f7
BrunoSabaLeega <bruno.saba@leega.com.br>
2023-03-09 15:51:07 -0300

    ajuste-front

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = express.Router();
 basePath = __dirname + "/views";
 
-routes.get('/', (request, response) => response.sendFile(basePath + "/home.html"))
+routes.get('/', (request, response) => response.sendFile(basePath + "/main.html"))
 routes.get('/main.html', (request, response) => response.sendFile(basePath + "/main.html"))
+routes.get('/login.html', (request, response) => response.sendFile(basePath + "/login.html"))
 
 
 module.exports = routes;
